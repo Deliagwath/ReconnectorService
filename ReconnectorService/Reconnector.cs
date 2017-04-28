c0e18ca098b813a81aa86c53c6bbe5e11983bc2e
Deliagwath <PPL3000@Gmail.com>
2017-04-28 13:21:43 +0700

    Initial Commit

---
+++ ReconnectorService/Reconnector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace ReconnectorService
+{
+    class Reconnector
+    {
+        public static void CheckNReconnect()
+        {
+            if (!TestConnection())
+            {
+                ExecuteCommand("refresh.bat");
+            }
+        }
+
+        private static bool TestConnection()
+        {
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 10000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            //process.OutputDataReceived += (object sender, DataReceivedEventArgs e) => Console.WriteLine("output>>" + e.Data);
+            //process.BeginOutputReadLine();
+
+            //process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) => Console.WriteLine("error>>" + e.Data);
+            //process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            //Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+        }
+    }
+}
